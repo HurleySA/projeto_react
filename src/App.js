d40753c2253167c3713a142c957b72811ca93084
HurleySA <matheus.eduardo.silva@ccc.ufcg.edu.br>
2021-09-14 14:08:17 -0300

    Finalizando prática Routers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Contato from "./Components/Contato";
+import Item from "./Components/Item";
 import styles from "./App.css";
 const App = () => {
   return (
@@ -12,7 +13,9 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Produtos />} />
-          <Route path="/Contato" element={<Contato />} />
+          <Route path="produtos" element={<Produtos />} />
+          <Route path="produto/:id" element={<Item />} />
+          <Route path="Contato" element={<Contato />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Produtos.module.css";
+import Head from "./Head";
 
 const Produtos = () => {
   const [produtos, setProdutos] = React.useState(null);
@@ -9,15 +10,15 @@ const Produtos = () => {
     fetch("https://ranekapi.origamid.dev/json/api/produto")
       .then((res) => res.json())
       .then((json) => setProdutos(json));
-    console.log(produtos);
   }, []);
 
   if (produtos === null) return null;
   return (
-    <section className={styles.produtos}>
+    <section className={`${styles.produtos} animeLeft`}>
+      <Head title="Home" />
       {produtos.map((produto) => (
         <Link to={`produto/${produto.id}`} key={produto.id}>
-          <img src={produto.fotos[0].src} />
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
           <h1 className={styles.nome}>{produto.nome}</h1>
         </Link>
       ))}

fe8d4affa3db54f041bba9e6a40ee4dbb7675721
HurleySA <matheus.eduardo.silva@ccc.ufcg.edu.br>
2021-09-14 12:55:26 -0300

    Página Contatos e Produtos

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Produtos from "./Components/Produtos";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Contato from "./Components/Contato";
+import styles from "./App.css";
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/Produtos" element={<Produtos />} />
           <Route path="/" element={<Produtos />} />
+          <Route path="/Contato" element={<Contato />} />
         </Routes>
         <Footer />
       </BrowserRouter>

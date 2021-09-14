69b503042ec5b8576d766a56eb2b4540ae44870b
HurleySA <matheus.eduardo.silva@ccc.ufcg.edu.br>
2021-09-14 09:07:55 -0300

    Iniciando prática Routes

--- src/App.js
+++ src/App.js
@@ -1,97 +1,19 @@
 import React from "react";
-import "./App.css";
+import Produtos from "./Components/Produtos";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 const App = () => {
-  const formFields = [
-    {
-      id: "nome",
-      label: "Nome",
-      type: "text",
-    },
-    {
-      id: "email",
-      label: "Email",
-      type: "email",
-    },
-    {
-      id: "senha",
-      label: "Senha",
-      type: "password",
-    },
-    {
-      id: "cep",
-      label: "Cep",
-      type: "text",
-    },
-    {
-      id: "rua",
-      label: "Rua",
-      type: "text",
-    },
-    {
-      id: "bairro",
-      label: "Bairro",
-      type: "text",
-    },
-    {
-      id: "cidade",
-      label: "Cidade",
-      type: "text",
-    },
-    {
-      id: "estado",
-      label: "Estado",
-      type: "text",
-    },
-  ];
-
-  const [form, setForm] = React.useState({
-    nome: "",
-    email: "",
-    senha: "",
-    cep: "",
-    rua: "",
-    bairro: "",
-    cidade: "",
-    estado: "",
-  });
-
-  const [response, setResponse] = React.useState(null);
-
-  const handleChange = ({ target }) => {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch("https://ranekapi.origamid.dev/json/api/usuario", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    }).then((res) => {
-      setResponse(res);
-    });
-  };
-
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        {formFields.map(({ id, label, type }) => (
-          <div key={id} className="labels">
-            <label htmlFor={id}> {label}</label>
-            <input
-              type={type}
-              id={id}
-              value={form[id]}
-              onChange={handleChange}
-            />
-          </div>
-        ))}
-        <button>Enviar</button>
-        {response && response.ok && <p>Formulário enviado</p>}
-      </form>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/Produtos" element={<Produtos />} />
+          <Route path="/" element={<Produtos />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
